157101909efe53f1ef158c8cf1abf75609c87446
Taylor Le <taylor.l.kingston@gmail.com>
2022-11-14 11:54:57 -0500

    set up react homepage

---
+++ app/javascript/react/components/AboutTile.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const AboutTile = props => {
+  return(
+    <div className='about callout'>
+      <h1>Count Your Plants!</h1>
+      <p>Did you know that plant diversity leads to gut microbiome diversity which in turn leads to better overall well being? ........ </p>
+    </div>
+  )
+}
+
+export default AboutTile
---
+++ app/javascript/react/components/Homepage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import AboutTile from './AboutTile.js'
+const Homepage = props => {
+  return (
+    <div className="produce-background">
+     <AboutTile />
+    </div>
+  )
+}
+
+export default Homepage
