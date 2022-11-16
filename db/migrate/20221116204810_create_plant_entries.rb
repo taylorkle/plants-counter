9d2af45bc8efd7b039ee8b69fed499007dca7102
Taylor Le <taylor.l.kingston@gmail.com>
2022-11-16 16:30:47 -0500

    edit tables and models

---
+++ db/migrate/20221116204810_create_plant_entries.rb
@@ -0,0 +1,8 @@
+class CreatePlantEntries < ActiveRecord::Migration[5.2]
+  def change
+    create_table :plant_entries do |t|
+      t.belongs_to :user, null: false
+      t.belongs_to :plant, null: false
+    end
+  end
+end
