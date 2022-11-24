868cbf7a12f4d0b28494df9196f4a81bfe54dff0
Taylor Le <taylor.l.kingston@gmail.com>
2022-11-23 19:23:27 -0500

    in progress

--- app/javascript/react/components/Homepage.js
+++ app/javascript/react/components/Homepage.js
@@ -27,16 +27,16 @@ const Homepage = props => {
 
   let button = null
   if (authentication) {
-    button = <button type="button"> <Link to='/plants'>Start Counting</Link></button>
+    button = <button className="button" type="button"> <Link to='/plants'>Start Counting</Link></button>
   } else {
-    button = <button><a href="/users/sign_in">Sign in to start counting</a></button>
+    button = <button className="button"><a href="/users/sign_in">Sign in to start counting</a></button>
   }
 
   return (
     <div className="produce-background grid-container">
       <div className="grid-x">
         <div className="about callout small-10 medium-8 large-6">
-          <h1>Count Your Plants!</h1>
+          <h1 className="secondary-text">Count Your Plants!</h1>
           <p>Did you know that plant diversity leads to gut microbiome diversity which in turn leads to better overall well being? ........ </p>
           {button}
         </div>
