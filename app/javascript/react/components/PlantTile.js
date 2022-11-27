a8f171693395e4a0a7d834dcb3c3837d602a1e62
Taylor Le <taylor.l.kingston@gmail.com>
2022-11-27 10:49:47 -0500

    button and grid styling

--- app/javascript/react/components/PlantTile.js
+++ app/javascript/react/components/PlantTile.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const PlantTile = props => {
   return(
-    <div className="cell small-6 medium-4 large-3 plant-tile">
+    <div className="cell small-4 medium-3 large-3 plant-tile">
         <p className="center-text">{props.name}</p>
         <img className="plant-display" src={`https://spoonacular.com/cdn/ingredients_100x100/${props.image}`} alt={props.name}/>
     </div>
