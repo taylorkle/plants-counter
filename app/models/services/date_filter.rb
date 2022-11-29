456e46e06deb5dc1f538dc2ef98627c3268b8737
Taylor Le <taylor.l.kingston@gmail.com>
2022-11-28 21:55:27 -0500

    in progress

--- app/models/services/date_filter.rb
+++ app/models/services/date_filter.rb
@@ -1,16 +1,20 @@
 class DateFilter
 
   def self.week_start
-    current_date = Date.today
-    return week_start = current_date.at_beginning_of_week(:sunday).beginning_of_day
+    current_date_time = DateTime.now  #returns local time
+    return week_start = current_date_time.at_beginning_of_week(:sunday).beginning_of_day
   end
 
   def self.week_end
-    current_date = Date.today
-    return week_end = current_date.at_end_of_week(:sunday).end_of_day
+    current_date_time = DateTime.now
+    return week_end = current_date_time.at_end_of_week(:sunday).end_of_day
   end
 
+  #use offset = Time.now.utc_offset to determine minutes off /60 /60
+  #(created_at.to_time - offset.hours).to_datetime
+
   def self.get_current_plants(plant_entries)
+    binding.pry
     current_plants = []
     plant_entries.each do |entry|
       if entry["created_at"] >= week_start && entry["created_at"] <= week_end
