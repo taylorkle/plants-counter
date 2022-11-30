2a122cd6ad2c2b0cb4613418d4c0e34668a4989a
Taylor Le <taylor.l.kingston@gmail.com>
2022-11-30 18:00:31 -0500

    close button in progress

--- app/javascript/react/components/PlantTile.js
+++ app/javascript/react/components/PlantTile.js
@@ -3,7 +3,8 @@ import React from 'react'
 const PlantTile = props => {
   return(
     <div className="cell small-4 medium-3 large-3 plant-tile">
-        <p className="center-text">{props.name}</p>
+        <p className="tiles">{props.name}</p>
+        <button type="button" id="close">X</button>
         <img className="plant-display" src={`https://spoonacular.com/cdn/ingredients_100x100/${props.image}`} alt={props.name}/>
     </div>
   )
