0aa0c18a6a8aea7c0d022b95067dcfc6f786ab75
Taylor Le <taylor.l.kingston@gmail.com>
2022-12-16 15:15:10 -0500

    Refactor error handling in plantentries controller

--- app/controllers/api/v1/plant_entries_controller.rb
+++ app/controllers/api/v1/plant_entries_controller.rb
@@ -4,30 +4,27 @@ class Api::V1::PlantEntriesController < ApiController
 
   def create
     user = current_user
-    plant_entry = nil
-    plant = nil
+    plant_entry = {}
+    plant = {}
     if Plant.where(api_id: plant_params["id"]).empty?
       plant = Plant.new(api_id: plant_params["id"], name: plant_params["name"], image: plant_params["image"])
       if !plant.save
-        render json: { error_status: true, error: plant.errors.full_messages.to_sentence }, status: 400
+        render json: {error_status: true, error: plant.errors.full_messages},status: 500 and return
       end
     else
       plant = Plant.find_by(name: plant_params["name"])
     end
 
     last_plant_entry = PlantEntry.where(user: user, plant: plant).order(:created_at).last
-    if plant && DateFilter.not_duplicate?(last_plant_entry)
+    if DateFilter.not_duplicate?(last_plant_entry)
       plant_entry = PlantEntry.new(plant: plant, user: user)
-    else
-      render json: { error_status: true, error: "Plant already added for current week" }, status: 400
-    end
-
-    if plant_entry
       if plant_entry.save
         render json: { plant: plant }
       else
-        render json: { error_status: true, error: plant_entry.errors.full_messages }, status: 400
+        render json: { error_status: true, error: plant_entry.errors.full_messages }, status: 500
       end
+    else
+      render json: { error_status: true, error: "Plant already added for current week" }, status: 400
     end
   end
 
